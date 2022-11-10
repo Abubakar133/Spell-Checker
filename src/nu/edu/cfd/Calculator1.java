9a61ebd65d6c162bd003fe79380c0ca41d600bf0
Abubakar133 <116784748+Abubakar133@users.noreply.github.com>
2022-11-10 21:51:51 +0500

    initial

---
+++ src/nu/edu/cfd/Calculator1.java
@@ -0,0 +1,12 @@
+package nu.edu.cfd;
+
+import java.awt.event.ActionListener;
+
+public class Calculator1 extends Frame implements ActionListener {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/nu/edu/cfd/calculator.java
@@ -0,0 +1,12 @@
+package nu.edu.cfd;
+
+import java.awt.event.ActionListener;
+
+public class calculator extends Frame implements ActionListener {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
